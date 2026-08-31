871126d43dbbdea06916cb712a1faf10dcd1ed54
Sumit Dhami <sumit342783@gmail.com>
2026-08-31 11:11:45 +0530

    Add files via upload

---
+++ Day18.c
@@ -0,0 +1,27 @@
+// Write a C program to input a non-negative integer and calculate its factorial using a for loop.
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    long long factorial = 1;
+
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        printf("Factorial is not defined for negative numbers.");
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            factorial = factorial * i;
+        }
+
+        printf("Factorial of %d = %lld", n, factorial);
+    }
+
+    return 0;
+}
